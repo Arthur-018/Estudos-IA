408e170c657b4de790725e44b756dd927a73c975
André Arthur Toledo Ferreira <Andre.Ferreira@ciazaffari.com.br>
2026-05-19 11:27:40 -0300

    Ultima aula do cursoGemini e Node.js: integrando sua aplicação com a API do Google

--- Gemini e Node integrando com API do Google/index.js
+++ Gemini e Node integrando com API do Google/index.js
@@ -1,35 +1,30 @@
-import { GoogleGenAI } from "@google/genai";
-import { fazerPergunta } from "./pergunta.js";
+import { fazerPergunta } from './pergunta.js';
+import { perguntar } from './perguntaLivre.js';
+import { consultar } from './consultaDestino.js';
+import { processaImagem } from './processaImagem.js';
+import { processaArquivoTexto } from './categorizador.js';
 
+async function principal() {
+  const escolha = await fazerPergunta(`Escolha uma das opções abaixo: \n
+  1. Fazer uma pergunta livre sobre um destino;
+  2. Comparação de destinos por categorias;
+  3. Ver informações com base em uma imagem;
+  4. Fazer a análise de sentimentos baseado em arquivo texto
+  \nOpção desejada: `);
 
-const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
-});
-
-async function main() {
-    try {
-        let prompt = "Voce é um site de viagens e deve responder somente sobre esse assunto" +
-        " Caso o usuário pergunte sobre algo diferente. Diga que não pode responder." +
-        " O usuário escolheu: ";
-        prompt += await fazerPergunta("Escreva qual lugar quer conhecer: ")
-        
-        const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: prompt,
-        });
-
-        console.log(response.text);
-        
-    } catch (error) {
-        console.log("Erro ao chamar a IA.");
-        
-        if (error.status === 503) {
-            console.log("O modelo está com alta demanda. tente novamente daqui a pouco.");
-        } else {
-            console.log(error.mensage);
-            
-        }
-    }
+  if (escolha === '1') {
+    await perguntar();
+  } else if (escolha === '2') {
+    await consultar();
+  } else if (escolha === '3') {
+    const imagem = await fazerPergunta("\nMe informe o caminho completo e nome da imagem: ")
+    await processaImagem(imagem);
+  } else if (escolha === '4') {
+    await processaArquivoTexto();
+  } else {
+    console.log('Escolha inválida.');
+  }
 }
 
-main();
+principal();
+
