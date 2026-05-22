0d715f22601bf7a27faa682b7eabe99686bf8474
André Arthur Toledo Ferreira <Andre.Ferreira@ciazaffari.com.br>
2026-05-22 11:02:03 -0300

    2 aula de Gemini e Node.js: desenvolva um Chatbot com a IA do Google

--- Gemini e Node.js desenvolva um Chatbot com a IA do Google/chat.js
+++ Gemini e Node.js desenvolva um Chatbot com a IA do Google/chat.js
@@ -1,34 +1,8 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { chat } from './inicializaChat.js';
 
-
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
-async function run() {
-  
-  const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro"});
-
-  const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [{ text: "Oi, adoro viajar!" }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Ah que legal, para onde deseja viajar?" }],
-      },
-    ],
-    generationConfig: {
-      maxOutputTokens: 100,
-    },
-  });
-
-  const msg = "Quero ir para o Canadá";
-
-  const result = await chat.sendMessage(msg);
+export async function executaChat(mensagem) {
+  console.log("Tamanho do histórico: " + (await chat.getHistory()).length);
+  const result = await chat.sendMessage(mensagem);
   const response = await result.response;
-  const text = response.text();
-  console.log(text);
+  return response.text();
 }
-
-run();
